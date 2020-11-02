cce5b2e630443c12f2c697f6f40e83648382820d
Jumana5 <Jumana5@users.noreply.github.com>
2020-11-02 21:47:13 +0300

    added user profile second layout for the second idea

---
+++ app/src/main/java/com/example/tbnaaproject/UserProfile2.java
@@ -0,0 +1,14 @@
+package com.example.tbnaaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfile2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_2);
+    }
+}
