a15e9d411425ecab742154cd6a210c27637a8a0b
Jumana5 <Jumana5@users.noreply.github.com>
2020-11-05 02:55:05 +0300

    fixed few errors here and there and added files for my interfaces

--- app/src/main/java/com/example/tbnaaproject/CatProfile.java
+++ app/src/main/java/com/example/tbnaaproject/CatProfile.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class catProfile extends AppCompatActivity {
+public class CatProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
