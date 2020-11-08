b8e590737bf4af7b9ca4e1e6d4d3379626ff71a1
biano <ibeeno.97@gmail.com>
2020-11-08 16:24:48 +0300

    cat card page edit

--- app/src/main/java/com/example/tbnaaproject/CatCards.java
+++ app/src/main/java/com/example/tbnaaproject/CatCards.java
@@ -2,7 +2,12 @@ package com.example.tbnaaproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class CatCards extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class CatCards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_cards);
+
+        AdapterView.OnItemSelectedListener OnCatSpinnerCL = new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+                ((TextView) parent.getChildAt(0)).setTextSize(15);
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        };
+
     }
 }
