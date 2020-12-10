7ae3e9a0fe6b4afc3846197ac70c00197c47ae6f
Maryam El-Aiat <42186759+MatyamJamal89@users.noreply.github.com>
2020-12-10 05:19:46 +0300

    Admin Table

--- app/src/main/java/com/example/tbnaaproject/TbnaaDatabase.java
+++ app/src/main/java/com/example/tbnaaproject/TbnaaDatabase.java
@@ -23,7 +23,7 @@ public class TbnaaDatabase  {
 
     //Cat table
     private static String catTableName = "Cat";
-    private static final String createCatTable = "CREATE TABLE Cat (catId INTEGER PRIMARY KEY AUTOINCREMENT" +
+    private static final String createCatTable = "CREATE TABLE Cat (catID INTEGER PRIMARY KEY AUTOINCREMENT" +
             ",catImage BLOB, catName TEXT,catAge TEXT, catCity TEXT, catGender TEXT,vaccinated TEXT, neutered TEXT" +
             ", healtheCare TEXT, catStory TEXT, isApproved TEXT, isAdoptted TEXT);";
 
@@ -164,6 +164,24 @@ public class TbnaaDatabase  {
         cv.put("uEmail", uEmail);
         cv.put("uPhone", uPhone);
         cv.put("uCity", uCity);
+
+        this.connect();
+        return database.insert(userTableName, null, cv);
+    }
+
+    public long addAdmin(byte[] aImage, String aPassword, String aFirstName,
+                        String aLastName, String aGender,
+                        String aEmail, String aPhone) {
+
+        ContentValues cv = new ContentValues();
+
+        cv.put("aPassword", aPassword);
+        cv.put("aImage", aImage);
+        cv.put("aFirstName", aFirstName);
+        cv.put("aLastName", aLastName);
+        cv.put("aGender", aGender);
+        cv.put("aEmail", aEmail);
+        cv.put("aPhone", aPhone);
 
         this.connect();
         return database.insert(userTableName, null, cv);
